cfe6bd73def50c5e431212219cbe9754c3e8319a
unknown <User@INFBN-02.up.ac.za>
2019-02-07 15:56:32 +0200

    I dont know what im doing

---
+++ Site.js
@@ -0,0 +1,4 @@
+function myFunction()
+{
+	alert("I'm really excited to learn how to use Github since I have always referenced the site. Plus I do BSc IT and my INF modules are always my favourite :)");
+}
